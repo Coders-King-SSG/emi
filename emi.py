18ba117000a97f7278f5c6ca51d1962bdfe44b9d
Coders-King-SSG <71335589+Coders-King-SSG@users.noreply.github.com>
2022-06-11 10:36:36 +0530

    Update emi.py

--- emi.py
+++ emi.py
@@ -6,14 +6,14 @@ ch = st.radio('Calculate', ('EMI', 'Outstanding Loan Balance'))
 if ch == 'EMI':
 	p = st.number_input('Principle amount : ')
 	r = st.number_input('Rate of interest : ', 0.0, 50.0, step=0.1)
-	n = st.number_input('Number of months : ', step=1.0)
+	n = st.number_input('Number of months : ', step=1)
 	if st.button('Calculate'):
 		emi = p * (r/100) * (((1+(r/100))**n)/(((1+(r/100))**n)-1))
 		st.write('EMI = ', round(emi, 2))
 elif ch == 'Outstanding Loan Balance':
 	p = st.number_input('Principle amount : ')
 	r = st.number_input('Rate of interest : ', 0.0, 50.0, step=1.0)
-	n = st.number_input('Number of months : ', step=1.0)
+	n = st.number_input('Number of months : ', step=1)
 	m = st.number_input('After how many months balance is calculated: ')
 	if st.button('Calculate'):
 		emi = (p * (((1+(r/100))**n)-((1+(r/100))**m)))/(((1+(r/100))**n)-1)
